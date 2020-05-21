80cb27e12d9484ce0dd8de59a1a8388883081535
mkotowski <mkotowski101@gmail.com>
2020-05-21 14:12:05 +0200

    Add dummy register info dump

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,26 @@ public:
 	~BearTerminal() = default;
 	void Open() {
 		terminal_open();
-		terminal_set("window: title='CHIP-8 Emulator', resizeable=true, minimum-size=66x34, size=66x34");
+		terminal_set("window: title='CHIP-8 Emulator', resizeable=true, minimum-size=66x34, size=100x34");
 	}
-	void Print() {
-		terminal_print(1, 1, "Hello, world!");
+	void PrintDebugInfo() {
+		terminal_printf(67, 1,  "[color=orange]%s:[/color] %s%s", "V0", "0xA2F0 " "[color=gray]", "mvi 2F0h");
+		terminal_printf(67, 2,  "[color=orange]%s:[/color] %s%s", "V1", "0x0000 " "[color=gray]", "nop");
+		terminal_printf(67, 3,  "[color=orange]%s:[/color] %s%s", "V2", "0x0000 " "[color=gray]", "nop");
+		terminal_printf(67, 4,  "[color=orange]%s:[/color] %s%s", "V3", "0x0000 " "[color=gray]", "nop");
+		terminal_printf(67, 5,  "[color=orange]%s:[/color] %s%s", "V4", "0x0000 " "[color=gray]", "nop");
+		terminal_printf(67, 6,  "[color=orange]%s:[/color] %s%s", "V5", "0x0000 " "[color=gray]", "nop");
+		terminal_printf(67, 7,  "[color=orange]%s:[/color] %s%s", "V6", "0x0000 " "[color=gray]", "nop");
+		terminal_printf(67, 8,  "[color=orange]%s:[/color] %s%s", "V7", "0x0000 " "[color=gray]", "nop");
+		terminal_printf(67, 9,  "[color=orange]%s:[/color] %s%s", "V8", "0x0000 " "[color=gray]", "nop");
+		terminal_printf(67, 10, "[color=orange]%s:[/color] %s%s", "V9", "0x0000 " "[color=gray]", "nop");
+		terminal_printf(67, 11, "[color=orange]%s:[/color] %s%s", "VA", "0x0000 " "[color=gray]", "nop");
+		terminal_printf(67, 12, "[color=orange]%s:[/color] %s%s", "VB", "0x0000 " "[color=gray]", "nop");
+		terminal_printf(67, 13, "[color=orange]%s:[/color] %s%s", "VC", "0x0000 " "[color=gray]", "nop");
+		terminal_printf(67, 14, "[color=orange]%s:[/color] %s%s", "VD", "0x0000 " "[color=gray]", "nop");
+		terminal_printf(67, 15, "[color=orange]%s:[/color] %s%s", "VE", "0x0000 " "[color=gray]", "nop");
+
+		terminal_printf(67, 17, "[color=orange]%s:[/color] %s%s", "PC", "0x0000 " "[color=gray]", "nop");
 	}
 	void GameScreenFrame() {
 		terminal_print //64 * 32
@@ -82,6 +98,10 @@ public:
 
 	void EmulateCycle() {
 		// Fetch Opcode
+		// Fetch one opcode from the memory at the location
+		// specofoed by the program counter (pc).
+		opcode = memory[pc] << 8 | memory[pc + 1];
+
 		// Decode Opcode
 		// Execute Opcode
 
@@ -90,6 +110,18 @@ public:
 
 	void SetKeys() {
 
+	}
+
+	void Clear() {
+		terminal_clear();
+	}
+
+	void ClearArea(int x, int y, int w, int h) {
+		terminal_clear_area(x, y, w, h);
+	}
+
+	void ClearGameScreen() {
+		ClearArea(1, 1, 64, 32);
 	}
 
 	bool drawFlag = false;
@@ -142,12 +174,19 @@ int main(int argc, char const *argv[])
 	BearTerminal terminal;
 	Chip8 chip8;
 
+	// Set up render system and register input callbacks
+	// Setup the graphics (window size, display mode, etc) and input system (bind callbacks)
+	// setupGraphics();
+	// setupInput();
+
 	terminal.Open();
 	terminal.GameScreenFrame();
-	terminal.Print();
+	terminal.PrintDebugInfo();
 	terminal.Refresh();
 
+	// Clear the memory, registers and screen
 	chip8.Initialize();
+	// Copy the program into the memory
 	chip8.LoadGame("pong");
 
 	// while (terminal_read() != TK_CLOSE);
@@ -155,26 +194,25 @@ int main(int argc, char const *argv[])
 	// emulation loop
 	while(!terminal.ShouldClose())
 	{
-		// emulate one cycle
+		// emulate one cycle of the system
 		chip8.EmulateCycle();
 
 		if (chip8.drawFlag)
 		{
 			// drawGraphics();
+			terminal.GameScreenFrame();
+			terminal.PrintDebugInfo();
+			terminal.Refresh();
 		}
 
 		chip8.SetKeys();
 
-		int key = terminal_read();
+		// int key = terminal_read();
 
-		if (key == TK_CLOSE || key == TK_ESCAPE)
-		{
-			break;
-		}
-
-		// terminal.GameScreenFrame();
-		// terminal.Print();
-		// terminal.Refresh();
+		// if (key == TK_CLOSE || key == TK_ESCAPE)
+		// {
+		// 	break;
+		// }
 	}
 
 	terminal.Close();
